bf32cdb58c9e84ab78e85b9892b9a3294355a104
yangman946 <clarenceyang284@gmail.com>
2021-06-24 19:51:32 +1000

    small update
    last one

--- website/SimpleStack/SimpleStack/settings.py
+++ website/SimpleStack/SimpleStack/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xsj6zq!h5r$#mpd$jwg2)ph6+9ag7y+%phi!*d7dgas0$sb9yt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "SimpleStack.pythonanywhere.com"]
 
 
 # Application definition
