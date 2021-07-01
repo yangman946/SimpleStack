28bb78a8b194c82350002aa0702e2f81fb0afaf6
yangman946 <clarenceyang284@gmail.com>
2021-07-01 17:10:54 +1000

    update to documentation

--- website/SimpleStack/SimpleStack/settings.py
+++ website/SimpleStack/SimpleStack/settings.py
@@ -120,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = '/home/SimpleStack/SimpleStack/website/SimpleStack/static'
+
